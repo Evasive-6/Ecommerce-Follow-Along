8fb739b310578c5e99324c173b355e35581b7c9a
Evasive-6 <albinshiju285@gmail.com>
2025-01-23 10:24:02 +0530

    added login func

--- backend/server.js
+++ backend/server.js
@@ -55,6 +55,22 @@ app.post("/create", async (req, res) => {
         res.send("Error occurred while creating user.");
     }
 });
+app.post("/login", async (req, res) => {
+    const { name, password } = req.body;
+  
+    const user = await usemodel.findOne({ name: name });
+    if (!user) {
+      return res.status(400).json({ message: "User not found" });
+    }
+  
+    const matchpass = await bcrypt.compare(password, user.password);
+  
+    if (matchpass) {
+      res.status(200).json({ message: "Login successful", user });
+    } else {
+      res.status(400).json({ message: "Invalid credentials" });
+    }
+  });
 
 app.listen(PORT, async () => {
     console.log(`Server is running on ${PORT}`);
